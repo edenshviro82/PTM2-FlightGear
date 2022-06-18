8eef7391890c5203708ffa8981a4608219c98479
amitara178 <amitara178@gmail.com>
2022-06-18 15:40:59 +0300

    Fix func and db Amit and alon

--- Backend/src/main/java/necessary_classes/FlightData.java
+++ Backend/src/main/java/necessary_classes/FlightData.java
@@ -3,34 +3,19 @@ package necessary_classes;
 import java.io.Serializable;
 import java.sql.Date;
 
+//flight data object contains all the information we want to collect from the simulator, at the end of flight
+//the object will be sent to backend
 public class FlightData implements Serializable {
     private Date startTime;
     private Date endTime;
-    private String planeId;
-    private String flightId;
-
-    public String getPlaneId() {
-        return planeId;
-    }
-
-    public void setPlaneId(String planeId) {
-        this.planeId = planeId;
-    }
-
-    public String getFlightId() {
-        return flightId;
-    }
-
-    public void setFlightId(String flightId) {
-        this.flightId = flightId;
-    }
-
     private float maxSpeed;
     private float maxAltitude;
     private double miles;
     private Location flyFrom;
     private Location flyTo;
     private TimeSeries ts;
+    private String planeId;
+    private String flightId;
 
     public FlightData() {
         this.ts = new TimeSeries();
@@ -38,10 +23,8 @@ public class FlightData implements Serializable {
     }
 
     //getters/////////////////////////////////////////////////////////////////////////
-    public java.sql.Date getStartTime() { return startTime; }
-    public Date getEndTime() {
-        return endTime;
-    }
+    public Date getStartTime() {return startTime;}
+    public Date getEndTime() {return endTime;}
     public float getMaxSpeed() {
         return maxSpeed;
     }
@@ -58,12 +41,12 @@ public class FlightData implements Serializable {
     public TimeSeries getTs() {
         return ts;
     }
+    public String getPlaneId() {return planeId;}
+    public String getFlightId() {return flightId;}
 
     //setters///////////////////////////////////////////////////////////////////////
-    public void setStartTime(Date startTime) { this.startTime = startTime; }
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
-    }
+    public void setStartTime(Date startTime) {this.startTime = startTime;}
+    public void setEndTime(Date endTime) {this.endTime = endTime;}
     public void setMaxSpeed(float maxSpeed) {
         this.maxSpeed = maxSpeed;
     }
@@ -80,4 +63,6 @@ public class FlightData implements Serializable {
     public void setFlyTo(Location flyTo) {
         this.flyTo = flyTo;
     }
+    public void setPlaneId(String planeId) {this.planeId = planeId;}
+    public void setFlightId(String flightId) {this.flightId = flightId;}
 }
