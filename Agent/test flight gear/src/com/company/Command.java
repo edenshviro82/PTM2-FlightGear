9ab5b03f33efa8ccaac3f9f51bf0e62a56973310
kfirs97 <kfirsitalkil@gmail.com>
2022-06-25 12:34:55 +0300

    new system architecture and agent class diagram

--- Agent/test flight gear/src/com/company/Command.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public abstract class Command {
-    public abstract void execute();
-}
--- Agent/test flight gear/src/com/company/DefaultIO.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public interface DefaultIO {
-
-    public String readText();
-    public void write(String text);
-    public float readVal();
-    public void write(float val);
-}
